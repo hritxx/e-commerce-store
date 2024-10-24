968b4747f7cfd6773f7e5d2a250ef3b8d844c8dc
hritxx <hriteekroy1869@gmail.com>
2024-10-24 21:51:08 +0530

    name update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
             to="/"
             className="text-2xl font-bold text-orange-400 items-center space-x-2 flex"
           >
-            E-Commerce
+            নব বাজার
           </Link>
 
           <nav className="flex flex-wrap items-center gap-4">
